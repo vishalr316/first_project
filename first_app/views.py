3eeddb547d64b77fd29fe7a5362694b1718e31bc
vishalr316 <vishalshawn@gmail.com>
2020-12-01 21:56:10 +0530

    updated as per MTV paradigm changes

--- first_app/views.py
+++ first_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Webpage, AccessRecord, Topic
 
 # Create your views here.
 
 def index(request):
-	my_dict = {'insert_me':'This text is from templates/first_app/index.html'}
+	webpages_list = AccessRecord.objects.order_by('date')
+	date_dict = {'access_records':webpages_list}
+	# my_dict = {'insert_me':'This text is from templates/first_app/index.html'}
 	# return HttpResponse("Wannacum!!")
-	return render(request,'first_app/index.html',context=my_dict)
+	return render(request,'first_app/index.html',context=date_dict)
 
 def noPattern(request):
 	return HttpResponse("This is triggered when no pattern is there for the first_app!")
